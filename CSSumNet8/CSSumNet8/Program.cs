c49015ea4e381564f83cea1f5aacff016e6566a1
Oli Sturm <oliver+commitsigning@oliversturm.com>
2023-11-10 23:02:44 +0000

    initial

---
+++ CSSumNet8/CSSumNet8/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CSSumNetFramework {
+  static class Program {
+    static Action<ulong> OutputResult(string prefix) => (ulong value) => Console.WriteLine($"{prefix}: {value}");
+
+    static ulong SumRecMaxTail(ulong max, ulong val, ulong result = 0) {
+      if (val < max)
+        return SumRecMaxTail(max, val + 1, result + val);
+      else
+        return result;
+    }
+
+    static IEnumerable<ulong> Range(ulong start, ulong count) {
+      for (ulong x = start; x < start + count; x++) {
+        yield return x;
+      }
+    }
+
+    public static void Main() {
+      var longList = Range(1, 300000).ToArray();
+      OutputResult("SumRecMaxTail long")(SumRecMaxTail(300000, 1));
+    }
+  }
+}
